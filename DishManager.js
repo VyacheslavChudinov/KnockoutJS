6a505e42ab72f8137a6182c82ee51bd7a15f6340
user2 <user@itechart-group.com>
2014-08-06 16:36:18 +0300

    But button added, dish amount added, design rewrited.

--- DishManager.js
+++ DishManager.js
@@ -1,22 +1,21 @@
-
 var model = [
-    new Dish("Heaven and Earth", 10, "image1"),
-    new Dish("Charcuterie board", 20, "image2"),
-    new Dish("Salt beef bagel", 30, "image2"),
-    new Dish("Wiener Schnitzel", 40, "image2"),
-    new Dish("Bacon naan", 50, "image2"),
+    new Dish('Heaven and Earth', 10, 'Images\\heaven.jpg'),
+    new Dish('Charcuterie board', 20, 'Images\\charc.jpg'),
+    new Dish('Salt beef bagel', 30, 'Images\\salt.jpg'),
+    new Dish('Wiener Schnitzel', 40, 'Images\\wiener.jpg'),
+    new Dish('Bacon naan', 50, 'Images\\bacon.jpg'),
 
-    new Dish("Ajo blanco", 5, "image2"),
-    new Dish("Beetroot ravioli", 10, "image2"),
-    new Dish("Buratta with blood orange and coriander seeds", 15, "image2"),
-    new Dish("Cabbage thoran", 20, "image2"),
-    new Dish("Chestnut tagiatelle", 25, "image2"),
+    new Dish('Ajo blanco', 5, 'Images\\ajo.jpg'),
+    new Dish('Beetroot ravioli', 10, 'Images\\beetroot.jpg'),
+    new Dish('Buratta with blood orange and coriander seeds', 15, 'Images\\buratta.jpg'),
+    new Dish('Cabbage thoran', 20, 'Images\\cabbage.jpg'),
+    new Dish('Chestnut tagiatelle', 25, 'Images\\chestnut.jpg'),
 
-    new Dish("Matcha green bubble tea", 13, "image2"),
-    new Dish("Blueberry eclairs", 31, "image2"),
-    new Dish("Barley sponge", 22, "image2"),
-    new Dish("Apple tarte tatin", 51, "image2"),
-    new Dish("Blood orange granita", 23, "image2")
+    new Dish('Matcha green bubble tea', 13, 'Images\\matcha.jpg'),
+    new Dish('Blueberry eclairs', 31, 'Images\\blueberry.jpg'),
+    new Dish('Barley sponge', 22, 'Images\\barley.jpg'),
+    new Dish('Apple tarte tatin', 51, 'Images\\apple.jpg'),
+    new Dish('Blood orange granita', 23, 'Images\\blood.jpg')
 ];
 
 var extendedModel = [];
@@ -27,7 +26,8 @@ for(var i = 0; i < model.length; ++i)
         price: model[i].price,
         imageURL: model[i].imageURL,
         index: i,
-        isChecked: ko.observable(false)
+        isChecked: ko.observable(false),
+        amount: ko.observable(1)
     });
 }
 
@@ -39,30 +39,23 @@ function dishesViewModel() {
         for(var i = 0; i < self.dishes().length; ++i)
         {
             var dish = self.dishes()[i];
-            result += +(dish.isChecked() ? dish.price : 0);
+            result += (dish.isChecked() ? dish.price : 0)*dish.amount();
         }
         return result;
     });
+    self.buttonClick = function(){
+        for(var i = 0; i < self.dishes().length; ++i)
+        {
+            self.dishes()[i].amount(1);
+            self.dishes()[i].isChecked(false);
+        }
+        $('.elementToPopUp').bPopup();
+
+    }
 }
 ko.applyBindings( new dishesViewModel() );
 
-
-/*var dishes = [
-    new Dish("Heaven and Earth", 10, "image1"),
-    new Dish("Charcuterie board", 20, "image2"),
-    new Dish("Salt beef bagel", 30, "image2"),
-    new Dish("Wiener Schnitzel", 40, "image2"),
-    new Dish("Bacon naan", 50, "image2"),
-
-    new Dish("Ajo blanco", 5, "image2"),
-    new Dish("Beetroot ravioli", 10, "image2"),
-    new Dish("Buratta with blood orange and coriander seeds", 15, "image2"),
-    new Dish("Cabbage thoran", 20, "image2"),
-    new Dish("Chestnut tagiatelle", 25, "image2"),
-
-    new Dish("Matcha green bubble tea", 13, "image2"),
-    new Dish("Blueberry eclairs", 31, "image2"),
-    new Dish("Barley sponge", 22, "image2"),
-    new Dish("Apple tarte tatin", 51, "image2"),
-    new Dish("Blood orange granita", 23, "image2")
-];*/
+ko.bindingHandlers.customBinding = {
+    init: function(element, valueAccessor){
+    }
+};
